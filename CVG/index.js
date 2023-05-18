a40b1df57cc2d4ebaa72b55f81aa853336d2ff4c
jopockel <joris.poc@gmail.com>
2023-05-18 15:09:56 +0200

    export get_code and copySolution

--- CVG/index.js
+++ CVG/index.js
@@ -1,6 +1,6 @@
 import {init, set_code} from './CVG';
 
 window.onload = _ => {
-  set_code("niks")
+  set_code("narayana: movl 4(%esp), %edi\nmovl $0, %eax\ncall narayana_hulp\nret\nnarayana_hulp: cmpl $2, %edi\njg .L4\nincl %eax\nret\n.L4: decl %edi\ncall narayana_hulp\nsubl $2, %edi\ncall narayana_hulp\naddl $3, %edi\nret")
   init();
 };
